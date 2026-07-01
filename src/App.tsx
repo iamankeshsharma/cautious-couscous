8b5b9e900822b14101ce620daca8f232531427bc
ANKESH <ankeshsharma@protonmail.com>
2026-07-01 21:25:12 +0530

    feat: vercel analytics fro react

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import "./App.css";
 import Canvas from "./components/canvas";
 import ToolBar from "./components/toolBar";
 import { Store } from "./context/StateContext";
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
